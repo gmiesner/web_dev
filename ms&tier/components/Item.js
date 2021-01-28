08a473fd25d9683bcf0b763d394e31b907ff6168
Gabriella Miesner <ella.miesner@gmail.com>
2021-01-27 19:32:51 -0600

    ms&tier

---
+++ ms&tier/components/Item.js
@@ -0,0 +1,67 @@
+import React from "react";
+import colors from "../data/colors.json";
+import courses from "../data/courses.json";
+import { Draggable } from "react-beautiful-dnd";
+
+export default class Item extends React.Component {
+  render() {
+    const color = colors[this.props.number.split(" ")[0]];
+    return (
+      <Draggable draggableId={this.props.number} index={this.props.index}>
+        {(provided) => (
+          <div
+            className="item"
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            ref={provided.innerRef}
+          >
+            <span
+              className="del"
+              onClick={() => this.props.removeCourse(this.props.number)}
+            >
+              ✕
+            </span>
+            <div>
+              <p className="number">{this.props.number}</p>
+              <p className="title">
+                {this.props.title.substring(0, 50) +
+                  (this.props.title.length >= 50 ? "..." : "")}
+              </p>
+            </div>
+            <style jsx>{`
+              .item {
+                border-radius: 2px;
+                padding: 8px;
+                margin: 0 8px 8px 0;
+                background: ${color};
+                max-width: 200px;
+                height: auto;
+                box-shadow: 0 3px 1px -2px rgb(0 0 0 / 20%),
+                  0 2px 2px 0 rgb(0 0 0 / 14%), 0 1px 5px 0 rgb(0 0 0 / 12%);
+                display: flex;
+                align-items: start;
+              }
+              p {
+                font-size: 1rem;
+                line-height: 1.5;
+              }
+              .del {
+                margin: 4px 7px 3px 2px;
+                font-size: 12px;
+                color: #ccc;
+                background: rgba(0, 0, 0, 0.4);
+                display: inline-block;
+                padding: 2px 4px;
+                border-radius: 40px;
+              }
+              .number {
+                font-weight: bold;
+                font-size: 1.1rem;
+              }
+            `}</style>
+          </div>
+        )}
+      </Draggable>
+    );
+  }
+}
---
+++ ms&tier/pages/index.js
@@ -0,0 +1,125 @@
+import Head from "next/head";
+import Row from "../components/Row";
+import React, { useState } from "react";
+import Search from "../components/Search";
+import { DragDropContext } from "react-beautiful-dnd";
+import { resetServerContext } from "react-beautiful-dnd";
+
+resetServerContext();
+
+const Index = () => {
+  const initialTiers = {
+    S: ["IS&T 1561", "IS&T 1562", "BIO SCI 1163"], // course numbers as IDs
+    A: [],
+    B: [],
+    C: [],
+    D: ["IS&T 1750"],
+  };
+  const tierColors = {
+    // thanks https://colorswall.com/palette/3297/
+    S: "#ff7f7e",
+    A: "#ffbf7f",
+    B: "#ffdf80",
+    C: "#feff7f",
+    D: "#beff7f",
+  };
+  const [tiers, setTiers] = useState(initialTiers);
+  const addClass = (number) => {
+    setTiers({
+      ...tiers,
+      S: [...tiers.S, number],
+    });
+  };
+  const removeCourse = (tier, number) => {
+    console.log(`removing ${number} from ${tier}`);
+    console.log(tiers[tier]);
+    setTiers({
+      ...tiers,
+      [tier]: tiers[tier].filter((c) => c != number),
+    });
+  };
+  const onDragEnd = (result) => {
+    const { destination, source, draggableId } = result;
+    if (!destination) {
+      return;
+    }
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    let newDestOrder = tiers[destination.droppableId];
+    let newSourceOrder = tiers[source.droppableId];
+    if (destination === source) {
+      newDestOrder.splice(source.index, 1);
+      newDestOrder.splice(destination.index, 0, draggableId);
+      setTiers({
+        ...tiers,
+        [destination.droppableId]: newDestOrder,
+      });
+    } else {
+      newSourceOrder.splice(source.index, 1);
+      newDestOrder.splice(destination.index, 0, draggableId);
+
+      setTiers({
+        ...tiers,
+        [destination.droppableId]: newDestOrder,
+        [source.droppableId]: newSourceOrder,
+      });
+    }
+  };
+  const META = {
+    title: `MITier: Tier rank MIT classes`,
+    description: `All in the title`,
+    url: `https://mitier.vercel.app`,
+    siteName: `MITier`,
+    image: `https://mitier.vercel.app/screenshot.jpg`,
+    imageAlt: `Example tier list of MIT classes`,
+  };
+  return (
+    <div>
+      <Head>
+        <title>{META.title}</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+        <meta property="og:title" content={META.title} />
+        <meta property="og:description" content={META.description} />
+        <meta property="og:image" content={META.image} />
+        <meta property="og:url" content={META.url} />
+        <meta name="twitter:card" content="summary_large_image" />
+
+        <meta property="og:site_name" content={META.siteName} />
+        <meta name="twitter:image" content={META.image} />
+        <meta name="twitter:image:alt" content={META.imageAlt} />
+      </Head>
+      <header>
+        <Search addClass={addClass} />
+      </header>
+      <section>
+        <DragDropContext onDragEnd={onDragEnd}>
+          {Object.keys(tiers).map((tierName) => {
+            const color = tierColors[tierName];
+            return (
+              <Row
+                removeCourse={removeCourse}
+                key={tierName}
+                title={tierName}
+                items={tiers[tierName]}
+                color={color}
+              />
+            );
+          })}
+        </DragDropContext>
+      </section>
+      <style jsx>{`
+        header {
+          margin-bottom: 2rem;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default Index;
